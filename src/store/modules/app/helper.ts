0c089d53114925557c54b8bd3d25df925583fa14
Percy <percy@railway.app>
2023-12-06 03:29:36 +0000

    Initial Commit

---
+++ src/store/modules/app/helper.ts
@@ -0,0 +1,41 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'appSetting'
+
+export type Theme = 'light' | 'dark' | 'auto'
+
+export type Language = 'en-US' | 'ko-KR' | 'ru-RU' | 'vi-VN' | 'zh-CN' | 'zh-TW'
+
+const languageMap: { [key: string]: Language } = {
+  'en': 'en-US',
+  'en-US': 'en-US',
+  'ko': 'ko-KR',
+  'ko-KR': 'ko-KR',
+  'ru': 'ru-RU',
+  'ru-RU': 'ru-RU',
+  'vi': 'vi-VN',
+  'vi-VN': 'vi-VN',
+  'zh': 'zh-CN',
+  'zh-CN': 'zh-CN',
+  'zh-TW': 'zh-TW',
+}
+
+export interface AppState {
+  siderCollapsed: boolean
+  theme: Theme
+  language: Language
+}
+
+export function defaultSetting(): AppState {
+  const language = languageMap[navigator.language]
+  return { siderCollapsed: false, theme: 'light', language }
+}
+
+export function getLocalSetting(): AppState {
+  const localSetting: AppState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalSetting(setting: AppState): void {
+  ss.set(LOCAL_NAME, setting)
+}
